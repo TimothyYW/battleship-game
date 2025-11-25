341b78620b80c8d39e95e4ded0a7a315bbad7617
Timothy <timothy.yeremia.wewengkang@gmail.com>
2025-11-25 22:23:51 +0700

    <feat> adding display rule function

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-HIDDEN_BOARD = [[' ' for _ in range(10)]]
-GUESS_BOARD = [[' ' for _ in range(10)]]
-
+def get_username():
+    """
+    function getting username for welcome message
+    """
+    while True:
+        user_name = input("Enter your name:")
+        if user_name:
+            print(f"Welcome to the battleship game {user_name}!")
+            return user_name
+        else:
+            print("Please enter your name.")
+            
+def guide_rule():
+    """
+    function to display rule for the game
+    """
+    print("rule of game: ")
+    print("there are 5 ships for player and computer")
+    print("give them coordinate for row and column from 0 to 10")
+    print("once 5 ships has been sunk,the game will be over")
+    print("have fun!")
